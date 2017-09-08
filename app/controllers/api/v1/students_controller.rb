08d7b70efadf2385a24ddeb9f91dcc8da0bc33a7
Gorkem Mulayim <gorkemmulayim@icloud.com>
2017-09-08 14:44:25 +0000

    Added student login functionality.

---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::StudentsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+  
+  def create
+    @student = Student.new(student_params)
+    
+    respond_to do |format|
+      if @student.save
+        format.json { render json: @student.to_json }
+      else
+        format.json { render json: { errors: @student.errors.full_messages } }
+      end
+    end
+  end
+  
+  private
+  def student_params
+    params.require(:student).permit(:name, :surname, :number)
+  end
+end
