42b28a1eb5333fbf728af950c9b695069a035acb
Gorkem Mulayim <gorkemmulayim@icloud.com>
2017-09-11 08:49:12 +0000

    Initialized devise admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :admins
   namespace :api do
     namespace :v1 do
       resources :students
     end
   end
+  
+  root 'home#index'
 end
