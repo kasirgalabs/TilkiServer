2bb4f6e514a2eb5f5cb5a39a56fa011c79557d08
Yunus İrhan <yunus.irhan@gmail.com>
2018-01-19 08:54:07 +0000

    Student sign-up bug fix

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  belongs_to :course_student
+  has_many :course_student
   has_many :test_answer
   has_many :classic_answer
   # Include default devise modules. Others available are:
