6d5d06095406c6f0f4a8213b4b6136f394171658
Kerim Siper <kerimsiper@gmail.com>
2018-01-29 20:18:16 +0000

    Updated Teachers Index Page Issue: #7. Added a sidebar for teachers. Resolved Teacher My Courses page Issue: #17. Also Resolved Teacher Upcoming Exams page, the data will be added when exam data is created. Issue:#16

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ end
 
 gem 'simple_form', '~> 3.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
-
+gem 'momentjs-rails'
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
     def home
+        if teacher_signed_in?
+            redirect_to teachers_index_path
+        end
         
+        if student_signed_in?
+            redirect_to student_index_path
+        end
     end
 end
