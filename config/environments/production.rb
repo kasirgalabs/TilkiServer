7060f55f059a94ce428112cd600f5e40cebaeb5e
Yunus İrhan <yunus.irhan@gmail.com>
2018-03-19 16:14:57 +0000

    heroku arkaplan gorsellerini gostermiyor, cozum denemesi 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,4 +91,5 @@ Rails.application.configure do
   
   config.serve_static_assets = true
   config.assets.compress = true
+  config.assets.digest = true
 end
