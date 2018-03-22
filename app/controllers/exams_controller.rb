8d919ff21681c43ebfd762a423daf56f031e4a92
Kerim Siper <kerimsiper@gmail.com>
2018-03-22 18:57:14 +0000

    Exam show bug corrected. Sidebar css bug corrected

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -8,12 +8,8 @@ class ExamsController < ApplicationController
       @questions = ExamPaper.where(:exam_id => @exam.id).all
       render 'teacher_show'
     elsif student_signed_in?
-      if DateTime.now.in_time_zone > (@exam.start_time - 10.minutes).in_time_zone
         @questions = ExamPaper.where(:exam_id => @exam.id).all
         render 'student_show'
-      else
-        redirect_back(fallback_location: root_path)
-      end
     else
       redirect_to root_path
     end
